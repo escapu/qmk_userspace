877cc0bc8092527404fa8b0907f1a7d205ea4ba3
Robert Koak <Robert.Koak@digital.homeoffice.gov.uk>
2024-08-05 00:13:39 +0100

    bongocat animation

--- keyboards/crkbd/keymaps/escapu/keymap.c
+++ keyboards/crkbd/keymaps/escapu/keymap.c
@@ -17,6 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "bongocat.h"
+#include "bongocat.c"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
@@ -136,12 +138,15 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (!is_keyboard_master()) {
-        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    if (is_keyboard_master()) {
+        return OLED_ROTATION_270;
+//        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    } else {
+        return OLED_ROTATION_180;
     }
-
     return rotation;
 }
+
 
 static void render_status(void) {
     // Host Keyboard Layer Status
@@ -173,24 +178,31 @@ static void render_status(void) {
 
 }
 
+//
+//static void render_logo(void) {
+//    static const char PROGMEM qmk_logo[] = {
+//        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+//        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+//        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+//    };
+//
+//    oled_write_P(qmk_logo, false);
+//}
 
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(qmk_logo, false);
-}
-
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
+bool oled_task_user(void) { // funcion indica que poner en cada OLED
+    if (is_keyboard_master()) { // OLED del master
+        oled_set_cursor(0, 0); // posiciona los pixeles en la pantalla
         render_status();  // Renders the current keyboard state (layer, lock, caps, scroll, etc)
-    } else {
-        render_logo();  // Renders a static logo
-        oled_scroll_left();  // Turns on scrolling
+
+//        oled_write("chow", false); // escribe en la pantalla
+    } else { // OLED del slave
+        render_anim_bongocat();
+        oled_set_cursor(0, 0);
+        oled_write_P(PSTR("WPM"), false);
+        oled_write(get_u8_str(get_current_wpm(), ' '), false);
     }
     return false;
 }
+
 #endif
+
